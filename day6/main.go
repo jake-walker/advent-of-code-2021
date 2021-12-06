af8fe83de94d80437988e3c7ace17f08dfd3026f
Jake Walker <hi@jakew.me>
2021-12-06 09:55:24 +0000

    Add day 6

--- day6/main.go
+++ day6/main.go
@@ -6,35 +6,51 @@ import (
 	"strings"
 )
 
-func CalculateFish(input []int) []int {
-	newFish := []int{}
+func CalculateFishTotal(fish []int, days int) int {
+	// for the second part, storing fish in a long list is super inefficient, so now just the counts are stored.
+	// each 'day' we can shift the array down by one like so (this is backwards to make it look nicer):
+	// Day | 8  7  6  5  4  3  2  1  0
+	// --- | -------------------------
+	//   0 |                1          -> 3
+	//   1 |                   1       -> 2
+	//   2 |                      1    -> 1
+	//   3 |                         1 -> 0
+	//   4 | 1     1                   -> 8,6
+	//   5 |    1     1                -> 7,5
 
-	for i := 0; i < len(input); i++ {
-		if input[i] <= 0 {
-			input[i] = 6
-			newFish = append(newFish, 8)
-			continue
-		}
+	counts := make([]int, 9)
 
-		input[i] = input[i] - 1
+	for _, i := range fish {
+		counts[i] += 1
 	}
 
-	return append(input, newFish...)
-}
+	for i := 1; i <= days; i++ {
+		// get the number of fish on zero days
+		zeros := counts[0]
 
-func CalculateFishLoop(fish []int, days int) []int {
-	for i := 0; i < days; i++ {
-		fish = CalculateFish(fish)
+		// delete the first element in the array to shift down the rest
+		counts = counts[1:]
+
+		// now set 8 and 6 for the new fish
+		// the counts array is one smaller now, so the 8 value needs to be appended
+		counts = append(counts, zeros)
+		counts[6] += zeros
 	}
-	return fish
+
+	sum := 0
+	for i := 0; i < len(counts); i++ {
+		sum += counts[i]
+	}
+
+	return sum
 }
 
 func main() {
 	input := helpers.StringSliceToIntSlice(strings.Split(helpers.GetInput("day6/input.txt"), ","))
 
-	part1 := CalculateFishLoop(input, 80)
-	fmt.Printf("part 1: %v\n", len(part1))
+	part1 := CalculateFishTotal(input, 80)
+	fmt.Printf("part 1: %v\n", part1)
 
-	part2 := CalculateFishLoop(input, 256)
-	fmt.Printf("part 2: %v\n", len(part2))
+	part2 := CalculateFishTotal(input, 256)
+	fmt.Printf("part 2: %v\n", part2)
 }
--- day6/main_test.go
+++ day6/main_test.go
@@ -1,16 +1,15 @@
 package main
 
 import (
-	"github.com/google/go-cmp/cmp"
 	"testing"
 )
 
-func TestCalculateFishLoop(t *testing.T) {
+func TestCalculateFishTotal(t *testing.T) {
 	fish := []int{3, 4, 3, 1, 2}
-	actual := CalculateFishLoop(fish, 18)
-	expected := []int{6, 0, 6, 4, 5, 6, 0, 1, 1, 2, 6, 0, 1, 1, 1, 2, 2, 3, 3, 4, 6, 7, 8, 8, 8, 8}
+	actual := CalculateFishTotal(fish, 18)
+	expected := 26
 
-	if diff := cmp.Diff(expected, actual); diff != "" {
-		t.Errorf("CalculateFishLoop() mismatch (-want +got):\n%s", diff)
+	if actual != expected {
+		t.Errorf("CalculateFishTotal() = %v, want %v", actual, expected)
 	}
 }

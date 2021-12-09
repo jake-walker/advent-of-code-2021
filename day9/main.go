57044c324c223910e1e97aedcf04ba93c7c59eaa
Jake Walker <hi@jakew.me>
2021-12-09 19:34:34 +0000

    Add day 9

--- day9/main.go
+++ day9/main.go
@@ -7,6 +7,11 @@ import (
 	"strconv"
 )
 
+type Point struct {
+	X int
+	Y int
+}
+
 func LoadInput(content string) [][]int {
 	lines := helpers.SplitLines(helpers.GetLines(content))
 	out := make([][]int, len(lines))
@@ -38,29 +43,43 @@ func MinMax(slice []int) (min, max int) {
 	return
 }
 
-func GetLowPoints(heightmap [][]int) []int {
-	out := []int{}
+func GetAdjacent(heightmap [][]int, point Point) []Point {
+	adjacent := []Point{}
+	if point.Y-1 >= 0 {
+		adjacent = append(adjacent, Point{point.X, point.Y - 1})
+	}
+	if point.Y+1 < len(heightmap) {
+		adjacent = append(adjacent, Point{point.X, point.Y + 1})
+	}
+	if point.X-1 >= 0 {
+		adjacent = append(adjacent, Point{point.X - 1, point.Y})
+	}
+	if point.X+1 < len(heightmap[0]) {
+		adjacent = append(adjacent, Point{point.X + 1, point.Y})
+	}
+	return adjacent
+}
+
+func GetAdjacentValue(heightmap [][]int, point Point) []int {
+	points := GetAdjacent(heightmap, point)
+	values := []int{}
+	for _, p := range points {
+		values = append(values, heightmap[p.Y][p.X])
+	}
+	return values
+}
+
+func GetLowPoints(heightmap [][]int) []Point {
+	out := []Point{}
 
 	for i := 0; i < len(heightmap); i++ {
 		for j := 0; j < len(heightmap[i]); j++ {
-			adjacent := []int{}
-			if i-1 >= 0 {
-				adjacent = append(adjacent, heightmap[i-1][j])
-			}
-			if i+1 < len(heightmap) {
-				adjacent = append(adjacent, heightmap[i+1][j])
-			}
-			if j-1 >= 0 {
-				adjacent = append(adjacent, heightmap[i][j-1])
-			}
-			if j+1 < len(heightmap[i]) {
-				adjacent = append(adjacent, heightmap[i][j+1])
-			}
+			adjacent := GetAdjacentValue(heightmap, Point{j, i})
 
 			min, _ := MinMax(adjacent)
 
 			if heightmap[i][j] < min {
-				out = append(out, heightmap[i][j])
+				out = append(out, Point{j, i})
 			}
 		}
 	}
@@ -68,19 +87,56 @@ func GetLowPoints(heightmap [][]int) []int {
 	return out
 }
 
-func GetTotalRiskLevel(points []int) int {
+func GetTotalRiskLevel(heightmap [][]int, points []Point) int {
 	total := 0
 	for _, point := range points {
-		total += point + 1
+		total += heightmap[point.Y][point.X] + 1
 	}
 	return total
+}
+
+func GetBasin(heightmap [][]int, point Point, checked map[Point]bool) map[Point]bool {
+	// get the adjacent values
+	adjacent := GetAdjacent(heightmap, point)
+
+	for _, p := range adjacent {
+		// skip this point if it has already been checked
+		if checked[p] == true {
+			continue
+		}
+
+		if heightmap[p.Y][p.X] == 9 {
+			continue
+		}
+
+		checked[p] = true
+		checked = GetBasin(heightmap, p, checked)
+	}
+
+	return checked
 }
 
 func main() {
 	input := helpers.GetInput("day9/input.txt")
 	heightmap := LoadInput(input)
-
 	lowPoints := GetLowPoints(heightmap)
-	part1 := GetTotalRiskLevel(lowPoints)
+
+	part1 := GetTotalRiskLevel(heightmap, lowPoints)
 	fmt.Printf("part 1: %v\n", part1)
+
+	first := 0
+	second := 0
+	third := 0
+	for _, point := range lowPoints {
+		basin := GetBasin(heightmap, point, map[Point]bool{})
+		length := len(basin)
+		if length > first {
+			first = length
+		} else if length > second {
+			second = length
+		} else if length > third {
+			third = length
+		}
+	}
+	fmt.Printf("part 2: %v\n", first*second*third)
 }
--- day9/main_test.go
+++ day9/main_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/google/go-cmp/cmp"
-	"sort"
 	"testing"
 )
 
@@ -32,12 +31,34 @@ func TestGetLowPoints(t *testing.T) {
 		{9, 8, 9, 9, 9, 6, 5, 6, 7, 8},
 	}
 	actual := GetLowPoints(heightmap)
-	expected := []int{1, 0, 5, 5}
-
-	sort.Ints(actual)
-	sort.Ints(expected)
+	expected := []Point{
+		{1, 0},
+		{9, 0},
+		{2, 2},
+		{6, 4},
+	}
 
 	if diff := cmp.Diff(actual, expected); diff != "" {
 		t.Errorf("GetLowPoints() =\n%v", diff)
+	}
+}
+
+func TestGetBasin(t *testing.T) {
+	heightmap := [][]int{
+		{2, 1, 9, 9, 9, 4, 3, 2, 1, 0},
+		{3, 9, 8, 7, 8, 9, 4, 9, 2, 1},
+		{9, 8, 5, 6, 7, 8, 9, 8, 9, 2},
+		{8, 7, 6, 7, 8, 9, 6, 7, 8, 9},
+		{9, 8, 9, 9, 9, 6, 5, 6, 7, 8},
+	}
+	actual := GetBasin(heightmap, Point{1, 0}, map[Point]bool{})
+	expected := map[Point]bool{
+		{0, 0}: true,
+		{0, 1}: true,
+		{1, 0}: true,
+	}
+
+	if diff := cmp.Diff(actual, expected); diff != "" {
+		t.Errorf("GetBasin() =\n%v", diff)
 	}
 }
